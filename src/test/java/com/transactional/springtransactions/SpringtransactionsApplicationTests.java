15ad96f658458df3cf31b48487afbbd2a36ca078
HGChamara <hgchamara1991@gmail.com>
2023-02-22 06:27:55 +0530

    inital code base

---
+++ src/test/java/com/transactional/springtransactions/SpringtransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transactional.springtransactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
